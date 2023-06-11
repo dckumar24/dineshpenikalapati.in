2c3bcd71ef7ed78a5705b9ed60c67ac1e6909d10
dckumar24 <dc8790351056@gmail.com>
2023-06-11 16:45:44 +0530

    added landing content and dark theme

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const App = () =>{
     return (
-        <h1>
-            Hey, This is Dinesh Kumar Penikalapati
+    <div className='container'>
+        <h1 style={{color:'white'}}>
+            Hey, This is Dinesh Kumar Penikalapati.
         </h1>
+        <p style={{color:'white', textAlign:'center'}}>This website is under development.</p>
+        <p style={{color:'white', textAlign:'center'}}>Thanks for visiting.</p>
+        
+        </div>
     )
 }
 
