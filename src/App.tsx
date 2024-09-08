2229550e760ea93b4ddbb0aad74617e4fe293b92
dckumar24 <dc8790351056@gmail.com>
2024-09-08 17:22:25 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import VariantA from "./components/templates/VariantA/VariantA"
 import { ThemeContext } from "./context/ThemeContext";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const AppWrapper:React.ElementType='div';
 
@@ -14,6 +15,8 @@ function App() {
     
     <AppWrapper className={theme==='dark'?' bg-[#1e1e1e] text-white':' text-black bg-slate-100'}>
 <SpeedInsights></SpeedInsights>
+<Analytics></Analytics>
+
         <VariantA ></VariantA> 
         
     </AppWrapper>
