da72c2d0aa91f8967296a8098e78adaf8919790e
dckumar24 <dc8790351056@gmail.com>
2026-09-04 22:08:18 +0530

    feat: clean old porfolio code

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { useEffect } from 'react'
-import { useCursor } from './hooks/useCursor'
-import Navbar from './components/ui/Navbar'
-import Footer from './components/ui/Footer'
-import HeroSection from './components/sections/HeroSection'
-import AboutSection from './components/sections/AboutSection'
-import SkillsSection from './components/sections/SkillsSection'
-import ExperienceSection from './components/sections/ExperienceSection'
-import EducationSection from './components/sections/EducationSection'
-import ProjectsSection from './components/sections/ProjectsSection'
-import ContactSection from './components/sections/ContactSection'
+import React from 'react'
 
-export default function App() {
-  const { cursorRef, ringRef } = useCursor()
-
-  // Hide default cursor on mount
-  useEffect(() => {
-    document.body.style.cursor = 'none'
-    return () => { document.body.style.cursor = 'auto' }
-  }, [])
-
+const App = () => {
   return (
-    <>
-      {/* Custom cursor */}
-      <div ref={cursorRef} className="custom-cursor" />
-      <div ref={ringRef} className="custom-cursor-ring" />
-
-      {/* App */}
-      <div className="relative bg-[#020408] min-h-screen">
-        <Navbar />
-        <main>
-          <HeroSection />
-          <AboutSection />
-          <SkillsSection />
-          <ExperienceSection />
-          <ProjectsSection />
-          <EducationSection />
-          <ContactSection />
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div>App</div>
   )
 }
+
+export default App
